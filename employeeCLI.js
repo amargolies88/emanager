a4725868102fa8215929f1a5994e6e7476b88f34
Alan Margolies <amargolies88@gmail.com>
2020-04-01 17:18:39 -0400

    added validation for role duplication

--- employeeCLI.js
+++ employeeCLI.js
@@ -1,7 +1,7 @@
 // Node Package Constants
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+const cTable = require("console.table");
 
 // MySQL Connection
 const connection = mysql.createConnection({
@@ -12,35 +12,91 @@ const connection = mysql.createConnection({
     database: "employeeDB"
 });
 
+//Connect to MySQL and execute first inquirer function
 connection.connect(err => {
     if (err) throw err;
-    startQuestions();
+    inqCreateOrView();
 });
 
-function startQuestions() {
+function inqCreateOrView() {
+    inquirer
+        .prompt({
+            name: "createNew",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Create", "View"]
+        })
+        .then(({ createNew }) => {
+            if (createNew === "Create") { inqCreateSelect() }
+            else if (createNew === "View") { inqViewSelect() }
+        });
+}
+
+function inqCreateSelect(again) {
     inquirer
         .prompt({
             name: "tableSelect",
             type: "list",
-            message: "Create new:",
+            message: "Create new... (choose one)",
             choices: ["Department", "Role", "Employee"]
         })
         .then(({ tableSelect }) => {
             console.log(tableSelect);
             switch (tableSelect) {
-                case "Department": inqDepartment(); break;
-                case "Role": inqRole(); break;
-                case "Employee": inqEmployee(); break;
+                case "Department": inqCreateDepartment(); break;
+                case "Role": inqCreateRole(); break;
+                case "Employee": inqCreateEmployee(); break;
             }
         });
 }
 
-function inqDepartment() {
+function inqViewSelect() {
+    inquirer
+        .prompt({
+            name: "viewSelect",
+            type: "list",
+            message: "What would you like to view?",
+            choices: ["Departments", "Roles", "Employees"]
+        })
+        .then(({ viewSelect }) => {
+            inqView(viewSelect.toLowerCase().slice(0, -1));
+        });
+}
+
+function inqView(option) {
+    console.log(option);
+    inquirer
+        .prompt({
+            name: "viewSelect",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [`View all ${option}s`, `Select ${option} to view`, `Search specific ${option}s to view`]
+        })
+        .then(({ viewSelect }) => {
+            switch (viewSelect.slice(0, 3)) {
+                case "Vie": viewAll(option); break;
+                case "Sel": viewList(option); break;
+                case "Sea": viewSearch(option); break;
+            }
+        });
+}
+
+function viewAll(option) {
+    let query = `SELECT * FROM ${option}`
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        inqCreateOrView();
+    });
+}
+
+function inqCreateDepartment() {
     inquirer
         .prompt({
             name: "depName",
             type: "input",
-            message: "Enter name of department."
+            message: "Enter name of department.",
+            validate: value => (value !== "") ? true : "Department input must not be empty"
         })
         .then(({ depName }) => {
             let query =
@@ -48,14 +104,39 @@ function inqDepartment() {
                     VALUES ("${depName}");`
             connection.query(query, (err, res) => {
                 if (err) throw err;
+                inqCreateOrView();
             });
         });
 }
 
-function inqRole() {
+function inqCreateRole() {
+    let roleNames = [];
+    let roleQuery = `SELECT * FROM role`;
+    connection.query(roleQuery, (err, res) => {
+        if (err) throw err;
+        res.forEach(row => {
+            roleNames.push(row.name);
+        });
+    });
+    inquirer
+        .prompt([
+            {
+                name: "roleName",
+                type: "input",
+                message: "Enter role name",
+                validate: value => {
+                    for (let i = 0; i < roleNames.length; i++) {
+                        if (value === roleNames[i]) return "Role already exists";
+                    }
+                    return true;
+                }
+            }
+        ])
+        .then(() => {
 
+        });
 }
 
-function inqEmployee() {
+function inqCreateEmployee() {
 
 }
