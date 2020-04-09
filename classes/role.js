5584acfa9f24d58f38904456d392932f952d3b5d
Alan Margolies <amargolies88@gmail.com>
2020-04-08 21:27:39 -0400

    many promises

---
+++ classes/role.js
@@ -0,0 +1,9 @@
+class role {
+    constructor(name, salary, departmentID) {
+        this.name = name;
+        this.salary = salary;
+        this.departmentID = departmentID;
+    }
+}
+
+module.exports = role;
---
+++ db/querydb.js
@@ -0,0 +1,91 @@
+const mysql = require('mysql');
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    getAll(table, args) {
+        return new Promise((resolve, reject) => {
+            let sql = `SELECT * FROM ${table};`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    getCol(col, table, args) {
+        return new Promise((resolve, reject) => {
+            let sql = `SELECT ${col} FROM ${table};`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    getWhere(table, statement, args) {
+        return new Promise((resolve, reject) => {
+            let sql = `SELECT * FROM ${table} WHERE ${statement};`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            })
+        });
+    }
+
+    getColWhere(col, table, statement, args) {
+        return new Promise((resolve, reject) => {
+            let sql = `SELECT ${col} FROM ${table} WHERE ${statement};`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            })
+        });
+    }
+
+    insertEmployee(employee, args) {
+        return new Promise((resolve, reject) => {
+            let sql =
+                `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES ("${employee.firstName}", "${employee.lastName}", ${employee.roleID}, ${employee.managerID});`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    insertRole(role, args) {
+        return new Promise((resolve, reject) => {
+            let sql =
+                `INSERT INTO role (name, salary, department_id)
+                VALUES ("${role.name}", ${role.salary}, ${role.department_id});`;
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+module.exports = Database;
